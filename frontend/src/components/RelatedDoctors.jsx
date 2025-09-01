3d25c6d0cdbf22e97c2eeb3ff6061030539f8d1b
Adityagautam2004 <adikr2002@gmail.com>
2025-09-01 17:01:28 +0530

    Doctors Section Completed

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/context';
+import { useNavigate } from 'react-router-dom';
 
-const TopDoctors = () => {
+
+export const RelatedDoctors = ({ docId, speciality }) => {
+
+    const {doctors} = useContext(AppContext);
+    const [relDoc, setRelDocs] = useState([]);
+
+    useEffect(() => {
+        if(doctors.length > 0 && speciality){
+            const doctorsData = doctors.filter(doc => doc.speciality === speciality && doc._id !== docId);
+            setRelDocs(doctorsData);
+        }
+    }, [docId, speciality, doctors]);
 
     const navigate = useNavigate();
-    const { doctors } = useContext(AppContext);
+
   return (
-    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium '>
             Top Doctors you can trust
         </h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive network of doctors you can trust.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-            {doctors.slice(0,12).map((item, index) => (
+            {relDoc.slice(0,5).map((item, index) => (
                 <div onClick={() => {navigate(`/appointments/${item._id}`); scrollTo(0, 0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                     <img className='bg-blue-50' src={item.image} alt="" />
                     <div className='p-4'>
@@ -32,5 +42,3 @@ const TopDoctors = () => {
     </div>
   )
 }
-
-export default TopDoctors
