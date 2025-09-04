bd56e3a3c771488ac4351a0a1d987073a39ce5f0
Adityagautam2004 <adikr2002@gmail.com>
2025-09-04 16:41:20 +0530

    Frontend part almost complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -52,6 +52,20 @@ const Navbar = () => {
           Create Account
         </button>
         }
+        <img onClick={() => setShowMenu(true)} className="w-6 md:hidden" src={assets.menu_icon} alt="" />
+        {/*---------- Mobile Menu ----------*/}
+        <div className={`mf:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all duration-300 fixed ${showMenu ? "fixed w-full" : " h-0 w-0"}`}>
+          <div className="flex items-center justify-between px-5 py-6">
+             <img className="w-36" src={assets.logo} alt="" />
+             <img className="w-7" onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+          </div>
+          <ul className="flex flex-col items-center mt-5 gap-2 text-lg font-medium px-5">
+            <NavLink  onClick={() => setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
+            <NavLink  onClick={() => setShowMenu(false)} to='/doctors'> <p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+            <NavLink  onClick={() => setShowMenu(false)} to='/about'> <p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+            <NavLink  onClick={() => setShowMenu(false)} to='/contact'> <p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
